bd6bf8940cabe902106560dff5fdeb1f4f4e069f
Bekbolat Kuralbayev <bkb@live.ru>
2020-10-13 16:14:11 +0600

    solve loops-for: forDict 2nd try

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -6,5 +6,8 @@ def reverse_dict(dict):
     for key in dict.keys():
         new_key = dict[key]
         new_value = key
-        rev_dict[str(new_key)] = str(new_value)
+        rev_dict[new_key] = new_value
     return rev_dict
+
+
+print(reverse_dict(dict))
